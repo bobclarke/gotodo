ed18a94d334aa8f1a862358e83e2a56ae0e33555
Bob Clarke <clarkeb@bobsmacbook.local>
2020-08-03 20:10:41 +0100

    Adding comment to server/models/models.go

--- server/models/models.go
+++ server/models/models.go
@@ -1,8 +1,10 @@
 package models
+
 import "go.mongodb.org/mongo-driver/bson/primitive"
+
+// ToDoList is a struct to hold information about a task
 type ToDoList struct {
- 
-  ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-  Task   string             `json:"task,omitempty"`
-  Status bool               `json:"status,omitempty"`
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Task   string             `json:"task,omitempty"`
+	Status bool               `json:"status,omitempty"`
 }
